9410f778967b402f65cc5fbda7d45958144894c4
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-14 18:48:27 +0000

    ok
    criando o form na pagina blade

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -10,10 +10,12 @@ class SupportController extends Controller
 {
     public function index(Support $support)
     {
-
         $supports = $support->all();
-
-
         return view('admin/supports/index', compact('supports'));
+    }
+    
+    public function create()
+    {
+        return view('admin/supports/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('supports/create', [SupportController::class]);
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
