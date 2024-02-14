c03a5f634513624bb2038cca3bf139755c7f37a0
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-14 20:20:45 +0000

    ok
    abreviando com @csrf

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,7 +1,8 @@
 <h1>Nova Dúvida</h1>
 
 <form action="{{ route('supports.store')}}" method="POST">
-    <input type="hidden" value="{{ csrf_token() }}" name="_token">
+    {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
+    @csrf()
     <input type="text" name="subject" placeholder="Assunto">
     <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
     <button type="submit">Enviar</button>
