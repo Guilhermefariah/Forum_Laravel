5b2b069e9fd082c6b07922a2cfa857a4cb20cefe
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-14 18:02:49 +0000

    ok
    criando a rota para exibicao do formulario de cadastro

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('supports/create', [SupportController::class]);
+
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
