b267cddd12f7009f3749af8c137f2f2c65fe51de
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-14 20:16:44 +0000

    ok
    adicionado name token

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,7 +1,7 @@
 <h1>Nova Dúvida</h1>
 
 <form action="{{ route('supports.store')}}" method="POST">
-    <input type="text" value="{{ csrf_token() }}">
+    <input type="text" value="{{ csrf_token() }}" name="_token">
     <input type="text" name="subject" placeholder="Assunto">
     <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
     <button type="submit">Enviar</button>
