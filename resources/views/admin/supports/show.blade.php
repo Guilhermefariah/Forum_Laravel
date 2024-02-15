3b36c6c350fa8430ce9a0e9ed242f817d212c16e
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-15 17:35:05 +0000

    show
    adicionando informacoes na pag show

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,1 +1,7 @@
-<h1></h1>
+<h1>Detalhes da Dúvida {{ $support->id }}</h1>
+
+<ul>
+    <li>Assunto: {{ $support->subject }}</li>
+    <li>Status: {{ $support->status }}</li>
+    <li>Descrição: {{ $support->body }}</li>
+</ul>
