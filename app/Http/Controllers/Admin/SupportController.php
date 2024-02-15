272900b6f34eb23c0e113974b90117e7d8f6769d
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-15 17:12:47 +0000

    return
    reduzindo a funcao de return

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -16,7 +16,7 @@ class SupportController extends Controller
     public function show(string|int $id)
     {
         if (!$support = Support::find($id)) {
-            return redirect()->back();
+            return back();
         };
         dd($support->subject);
     }   
