505de888442beb3c6ecc0e025d1fff905e4edc30
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-17 18:06:34 +0000

    validacoes
    helper old para persistir com os form preenchido

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -10,7 +10,7 @@
 <form action="{{ route('supports.store')}}" method="POST">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
     @csrf()
-    <input type="text" name="subject" placeholder="Assunto">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
