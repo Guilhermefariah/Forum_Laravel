8b7561d2a679acf8d273888f26120591c8cae14f
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-17 18:00:22 +0000

    validacoes
    em create adicionado uma variavel  em caso de algum erro

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,5 +1,12 @@
 <h1>Nova Dúvida</h1>
 
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
+
 <form action="{{ route('supports.store')}}" method="POST">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
     @csrf()
