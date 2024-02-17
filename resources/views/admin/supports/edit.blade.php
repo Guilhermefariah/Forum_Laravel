8226207e494150398d8e41db4f8f8b2d56e4eb87
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-17 16:06:09 +0000

    edit
    method put  que por padrao esta por POST

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -3,6 +3,7 @@
 <form action="{{ route('supports.update', $support->id)}}" method="POST">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
     @csrf()
+    <input type="text" value="PUT" name="_method">
     <input type="text" name="subject" value="{{ $support->subject }}" placeholder="Assunto">
     <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ $support->body}}</textarea>
     <button type="submit">Enviar</button>
