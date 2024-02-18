26930993d2819cb8348f957c64cb5e8a5648fe9c
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-18 16:44:46 +0000

    interface
    adicionando funcao new

--- app/Repositories/SupportRepositoryInterface.php
+++ app/Repositories/SupportRepositoryInterface.php
@@ -1,6 +1,7 @@
 <?php
     namespace App\Repositories;
 
+    use App\DTO\CreateSupportDTO;
     use stdClass;
 
     interface SupportRepositoryInterface
@@ -8,4 +9,5 @@
         public function getAll(string $filter = null): array;
         public function findOne(string $id): stdClass | null;
         public function delete(string $id): void;
+        public function new(CreateSupportDTO $dto): stdClass | null; 
     }
