363927be8b20c85682c635fccdd007fb1836fa02
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-18 16:33:03 +0000

    interface
    adicionando o delete em repository

--- app/Repositories/SupportRepositoryInterface.php
+++ app/Repositories/SupportRepositoryInterface.php
@@ -7,4 +7,5 @@
     {
         public function getAll(string $filter = null): array;
         public function findOne(string $id): stdClass | null;
+        public function delete(string $id): void;
     }
