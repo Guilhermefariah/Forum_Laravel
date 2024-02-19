4ed7dc33b5371b1269b398292f88f290975808e3
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-19 20:26:35 +0000

    form
    subject se existir se nao retorna o valor antigo

--- resources/views/admin/supports/partials/form.blade.php
+++ resources/views/admin/supports/partials/form.blade.php
@@ -1,4 +1,7 @@
 @csrf()
-<input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
-<textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
+<input type="text" name="subject" placeholder="Assunto" 
+value="{{ $support->subject ??  old('subject') }}">
+
+<textarea name="body" cols="30" rows="5" 
+placeholder="Descrição">{{ $support->body ?? old('body') }}</textarea>
 <button type="submit">Enviar</button>
