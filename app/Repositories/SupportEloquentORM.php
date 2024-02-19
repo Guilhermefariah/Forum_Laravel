e4d61c6a97621229293b1dc043b3163b4ca56dd6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-19 13:19:49 +0000

    ORM
    adicionando objeto model

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -2,10 +2,17 @@
 
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
     class SupportEloquentORM implements SupportRepositoryInterface
     {
+        public function __construct(
+            protected Support $model
+        )
+        {
+            
+        }
         public function getAll(string $filter = null): array
         {
 
