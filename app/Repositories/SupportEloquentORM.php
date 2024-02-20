122ac64381ef576aa93b599bdab7ac90a4ce210f
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-20 17:28:40 +0000

    paginate
    implementando novo metodo paginate, passando os tipo expecifico dos parametros

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -12,9 +12,20 @@
     {
         public function __construct(
             protected Support $model
-        )
+        ){}
+        
+        public function paginate(int $page = 1, $totalPerPage = 15, string $filter = null): PaginationInterface
         {
-            
+            $result =  $this->model
+                        ->where(function ($query) use ($filter) {
+                            if($filter){
+                                $query->where('subject', $filter);
+                                $query->orWhere('body', 'like', "%{$filter}%");
+
+                            }
+                        })
+                        ->paginate($totalPerPage, ['*'], 'page', $page);
+                             
         }
 
         public function getAll(string $filter = null): array
