d3e2ada3f970de9157a588c4b9aad2fd6e637ae8
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-21 14:25:16 +0000

    Presenter
    passando os metodos para a class

--- app/Repositories/PaginationPresenter.php
+++ app/Repositories/PaginationPresenter.php
@@ -3,5 +3,12 @@
 
     class PaginationPresenter implements PaginationInterface
     {
-        
+            //@return stdClass[]
+            public function items(): array;
+            public function total(): int;
+            public function isFirstPage(): bool;
+            public function isLastPage(): bool;
+            public function currentPage(): int;
+            public function getNumberNextPage(): int;
+            public function getNumberPreviouspage(): int;
     }
