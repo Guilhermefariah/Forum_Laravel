8b1c5eb5dc539ae017c6bb649868d187bee94023
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-25 18:44:54 +0000

    REST
    adicionando a rota resource para declarar todas as rotas

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Enums\SupportStatus;
 use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/test', function(){
-    dd(array_column(SupportStatus::cases(), 'name'));
-});
-
+ 
+// Route::resource('/supports', SupportController::class);
 Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
 Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
 Route::get('/supports/{id}/edit', [SupportController::class,'edit'])->name('supports.edit');
