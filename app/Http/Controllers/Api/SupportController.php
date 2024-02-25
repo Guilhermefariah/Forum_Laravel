35b9dc200b9ec9a7ce62fef58ebb086e0773f5a6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-25 21:43:03 +0000

    Api
    return novo objeto encontrado

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -44,6 +44,8 @@ class SupportController extends Controller
         if(!$support = $this->service->findOne($id)){
             return response()->json(['error' => 'Not Found'], Response::HTTP_NOT_FOUND);
         }
+
+        return new SupportResource($support);
     }
 
     /**
