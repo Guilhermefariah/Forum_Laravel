6b2bb810fe34184ddf53576ec95009a6c359e7c3
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-25 22:07:40 +0000

    Api
    deu certo return 204

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -59,12 +59,10 @@ class SupportController extends Controller
      */
     public function destroy(string $id)
     {
-        if (!$this->service->findOne($id)) {
+        if (!$support = $this->service->findOne($id)) {
             return response()->json(['error' => 'Not Found'], Response::HTTP_NOT_FOUND);
         }
 
         $this->service->delete($id);
-
-        return response()->json([], 204);
     }
 }
