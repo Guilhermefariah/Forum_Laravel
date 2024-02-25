261e1e011918ccbe2e4ad74f94c7fc6b5d18a0d8
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-25 20:31:56 +0000

    Resources
    return assunto ou subject

--- app/Http/Resources/SupportResource.php
+++ app/Http/Resources/SupportResource.php
@@ -14,6 +14,9 @@ class SupportResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'identify' => $this->id,
+            'subject' => $this->subject,
+        ];
     }
 }
