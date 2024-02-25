1ce3edf27e0d401fa460420a58346ee2e8a0d742
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-25 20:38:11 +0000

    Resources
    atualizando formato da  data

--- app/Http/Resources/SupportResource.php
+++ app/Http/Resources/SupportResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,7 +19,7 @@ class SupportResource extends JsonResource
             'identify' => $this->id,
             'subject' => $this->subject,
             'content' => $this->body,
-            'dt_created' => $this->created_at
+            'dt_created' => Carbon::make($this->created_at)->format('Y-m-d H:i:s')
         ];
     }
 }
