fc72bbd3736d3ff5ed0e6b46af6daec38f8a0431
Guilhermefaria321 <guilhermefaria321@gmail.com>
2024-02-27 22:21:04 +0000

    layouts
    adicionado duvidas

--- resources/views/admin/supports/partials/header.blade.php
+++ resources/views/admin/supports/partials/header.blade.php
@@ -1,5 +1,7 @@
 <div>
     <div class="flex items-center gap x-3">
-        <h1 class="text-lg text-black-500">Fórum</h1>
+        <h1 class="text-lg text-black-800">Fórum</h1>
+
+        <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvidas</span>
     </div>
 </div>
