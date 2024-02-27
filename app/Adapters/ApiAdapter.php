85c6e6178d20a7987cf2ef16ab4eb601887fbe4e
Guilhermefaria321 <guilhermefaria321@gmail.com>
2024-02-27 16:57:54 +0000

    Adapters
    adicionando as propriedades que serao convertidas para json

--- app/Adapters/ApiAdapter.php
+++ app/Adapters/ApiAdapter.php
@@ -1,10 +1,12 @@
 <?php
     namespace App\Addapters;
 
+use App\Repositories\PaginationInterface;
+
     class ApiAdapter
     {
-        public static function toJson()
+        public static function toJson(PaginationInterface $data)
         {
-            
+
         }
     }
