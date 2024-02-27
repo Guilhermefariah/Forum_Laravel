97309300aaf2390c0b8545cb92d0bc099c53acee
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-27 13:11:25 +0000

    UPDATE
    id ou request id

--- app/DTO/Supports/UpdateSupportDTO.php
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -16,7 +16,7 @@ use App\Http\Requests\StoreUpdateSupport;
         public static function makeFromRequest(StoreUpdateSupport $request, string $id = null): self
         {
             return new self(
-                $request->id,
+                $id ?? $request->id,
                 $request->subject,
                 SupportStatus::A,
                 $request->body
