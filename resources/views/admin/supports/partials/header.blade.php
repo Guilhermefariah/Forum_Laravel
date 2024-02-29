db4e0dab6e0cd483afcc656580e401e29b947449
Guilhermefariah <guilhermefaria321@gmail.com>
2024-02-29 00:47:52 +0000

    Header
    passando para verde

--- resources/views/admin/supports/partials/header.blade.php
+++ resources/views/admin/supports/partials/header.blade.php
@@ -2,6 +2,6 @@
     <div class="flex items-center gap x-3">
         <h1 class="text-lg text-black-800">Fórum</h1>
 
-        <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvidas</span>
+        <span class="px-3 py-1 text-xs text-blue-600 bg-green-300 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvidas</span>
     </div>
 </div>
