020c1df0b5cf86b80e6316632890bd5c00791fa0
Guilhermefariah <guilhermefaria321@gmail.com>
2024-03-02 23:51:25 +0000

    INDEX
    foreach passando items para support

--- resources/views/admin/supports/partials/content.blade.php
+++ resources/views/admin/supports/partials/content.blade.php
@@ -24,6 +24,42 @@
                             </th>
                         </tr>
                     </thead>
+                    <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    @foreach ($supports->items() as $support)
+                        <tr>
+                            <td class="px-4 py-2 text-sm font-medium whitespace-nowrap dark:text-white">
+                            </td>
+                            <td class="px-12 py-2 text-sm font-medium whitespace-nowrap">
+                                
+                            </td>
+                            <td class="px-4 py-2 text-sm whitespace-nowrap text-gray-500 dark:text-gray-400">
+                                
+                            </td>
+                            <td class="px-4 py-2 text-sm whitespace-nowrap">
+                                <div class="flex items-center">
+                                    
+                                        @if ($loop->index < 4)
+                                            <div class="object-cover w-6 h-6 -mx-1 border-2 border-white rounded-full dark:border-gray-700 shrink-0 bg-green-500"></div>
+                                        @endif
+                                    
+                                </div>
+                            </td>
+
+                            <td class="px-4 py-2 text-sm whitespace-nowrap flex">
+                               
+                                <a href="" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg">
+                                    Editar
+                                </a>
+                            
+                                <a href="" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                         @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
