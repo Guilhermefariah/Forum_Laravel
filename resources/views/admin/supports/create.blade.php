a7af28fb39b8e7c6360c7b11d1a6b466de8d7a17
Guilhermefariah <guilhermefaria321@gmail.com>
2024-03-14 21:16:27 -0300

    Create
    passando section content

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,10 @@
 @extends('admin.layouts.app')
 <h1>Nova Dúvida</h1>
 
+@section('content')
 <x-alert/>
 
 <form action="{{ route('supports.store') }}" method="POST">
     @include('admin.supports.partials.form')
 </form>
+@endsection
